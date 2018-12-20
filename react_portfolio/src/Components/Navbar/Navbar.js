2c31e9f9548c46601d566998d0fe99aff4bf5809
Matt Gasper <mrgasper27@gmail.com>
2018-12-20 10:43:37 -0600

    Added links in index.html. Changed navbar links and icons

--- react_portfolio/src/Components/Navbar/Navbar.js
+++ react_portfolio/src/Components/Navbar/Navbar.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Navbar, NavbarBrand, NavbarItem, Icon, NavbarMenu, NavbarLink, NavbarDropdown, NavbarEnd, brand, NavbarBurger, NavbarStart, NavbarDivider, Field, Control, Button } from 'bloomer';
+import { Navbar, NavbarBrand, NavbarItem, Icon, NavbarMenu, NavbarLink, NavbarDropdown, NavbarEnd, brand, NavbarBurger, NavbarStart} from 'bloomer';
 
 class Nav extends React.Component  {
 
-    state = {
+     state = {
         isActive: false
-    }
+     }
 
     render() {
     return (
@@ -14,13 +14,14 @@ class Nav extends React.Component  {
 
         <NavbarBrand>
             <NavbarItem>
-                <img src={brand} style={{ marginRight: 5 }} /> Bloomer
+                <img src={brand} style={{ marginRight: 5 }} /> Matthew Gasper
             </NavbarItem>
-            <NavbarItem isHidden='desktop'>
-                <Icon className='fa fa-github' />
+            {/* GitHub/LinkedIn links on Mobile */}
+            <NavbarItem isHidden='desktop' href="https://github.com/mattypockets" target="_blank">
+                <Icon className='fab fa-github' />
             </NavbarItem>
-            <NavbarItem isHidden='desktop'>
-                <Icon className='fa fa-twitter' style={{ color: '#55acee' }} />
+            <NavbarItem isHidden='desktop' href="https://linkedin.com/in/matthew-gasper" target="_blank">
+                <Icon className='fab fa-linkedin' />
             </NavbarItem>
             <NavbarBurger isActive={this.state.isActive} onClick={this.onClickNav} />
         </NavbarBrand>
@@ -31,31 +32,21 @@ class Nav extends React.Component  {
                 <NavbarItem hasDropdown isHoverable>
                     <NavbarLink href='#/documentation'>Documentation</NavbarLink>
                     <NavbarDropdown>
-                        <NavbarItem href='#/'>One A</NavbarItem>
-                        <NavbarItem href='#/'>Two B</NavbarItem>
-                        <NavbarDivider />
-                        <NavbarItem href='#/'>Two A</NavbarItem>
+                        <NavbarItem href='#/'>Home</NavbarItem>
+                        <NavbarItem href='#/'>About</NavbarItem>
+                        <NavbarItem href='#/'>Portfolio</NavbarItem>
+                        <NavbarItem href='#/'>Contact</NavbarItem>
                     </NavbarDropdown>
                 </NavbarItem>
             </NavbarStart>
+
+            {/* Github/LinkedIn links */}
             <NavbarEnd>
-                <NavbarItem href="https://github.com/AlgusDark/bloomer" isHidden='touch'>
-                    <Icon className='fa fa-github' />
+                <NavbarItem href="https://github.com/mattypockets" target="_blank" isHidden='touch'>
+                    <Icon className='fab fa-github' />
                 </NavbarItem>
-                <NavbarItem href="https://twitter.com/AlgusDark" isHidden='touch'>
-                    <Icon className='fa fa-twitter' style={{ color: '#55acee' }} />
-                </NavbarItem>
-                <NavbarItem>
-                    <Field isGrouped>
-                        <Control>
-                            <Button id="twitter" data-social-network="Twitter" data-social-action="tweet"
-                                data-social-target="http://bloomer.js.org" target="_blank" href="https://twitter.com/intent/tweet?text=bloomer:
-                                a set of React Stateless Components for bulma.io&amp;url=http://bloomer.js.org&amp;via=AlgusDark">
-                                <Icon className="fa fa-twitter" />
-                                <span>Tweet</span>
-                            </Button>
-                        </Control>
-                    </Field>
+                <NavbarItem href="https://linkedin.com/in/matthew-gasper" target="_blank" isHidden='touch'>
+                    <Icon className='fab fa-linkedin' />
                 </NavbarItem>
             </NavbarEnd>
         </NavbarMenu>
