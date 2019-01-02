3712e827fbd759b17310f0edead8a5360c3e928b
Matt Gasper <mrgasper27@gmail.com>
2019-01-02 12:22:43 -0600

    Added projects to app.js

--- react_portfolio/src/App.js
+++ react_portfolio/src/App.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import Nav from './Components/Navbar';
 import Foot from './Components/Footer';
+import Projects from './Components/Projects';
+import projectInfo from './projects.json';
 
-const App = () => (
+class App extends Component {
+
+state = {
+  projectInfo
+}
+
+render() {
+  return (
+    <div>
+      <Nav />
+      {this.state.projectInfo.map(project => (
+        <Projects
+          title = {project.title}
+          image = {project.image}
+          github = {project.github}
+          deployed = {project.deployed}
+          description = {project.description}
+        /> 
+      ))}
+      <Foot />
+    </div>
+  )
+}
   
-  <div>
-    <Nav />
-    <Foot />
-  </div>
 
-)
+}
 
 export default App;
